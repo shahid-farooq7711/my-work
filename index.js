f7a1d1dfbf71e8533e47713d333455c4916b9238
shahid-farooq7711 <55952567+shahid-farooq7711@users.noreply.github.com>
2020-08-22 20:34:05 +0500

    init setup

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('Helo Pakistan');
+//houres 
+// if the 
+// let hours = 24
+// if (hours>=6 && hours<12) {
+//   prompt('GoodMorning');
+// }
+// else if (hours>=12 && hours<18){
+//     prompt('Good evenin');
+// }
+// else if (hours<=18 && hours<24){
+//     prompt('Good night');
+// }
+// else
+// prompt('Tahjud');
+// for ( let i=0; i<=5; i++)
+// if  (i % 2 == 0) console.log(i);
+// let i = 0;
+// while(i<=8){
+//     if  (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+// // do while 
+// let i= 9 ;
+// do {
+//     if  (i % 2 !== 0) console.log(i);
+//    i++;  
+// } while(i<=5);  
+
+// object 
+let circle= {
+    radious:1,
+    location: {
+        x:1,
+        y:1
+    },
+    visible: true,
+    draw: function() {
+console.log('draw');
+}
+};
+circle.draw();
